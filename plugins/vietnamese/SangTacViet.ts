d83670c65fcfd43438d7f3267a0767d75cd528da
Elysia <71698422+aiko-chan-ai@users.noreply.github.com>
2026-05-19 20:57:25 +0700

    Update SangTacViet.ts

--- plugins/vietnamese/SangTacViet.ts
+++ plugins/vietnamese/SangTacViet.ts
@@ -193,47 +193,47 @@ function detectHostFromUrl(
 const GLYPH_MAP: Record<string, string> = {};
 // prettier-ignore
 ([
-  [0xE01B,'A'],[0xE01E,'y'],[0xE05F,'3'],[0xE063,'z'],[0xE06B,'K'],[0xE06C,'t'],
-  [0xE089,'l'],[0xE0D5,'S'],[0xE0D6,'T'],[0xE100,'o'],[0xE101,'P'],[0xE116,'4'],
-  [0xE122,'W'],[0xE124,'Z'],[0xE14B,'J'],[0xE160,'e'],[0xE184,'O'],[0xE186,'D'],
-  [0xE1A4,'f'],[0xE1AD,'e'],[0xE1B4,'k'],[0xE1B8,'f'],[0xE1BF,'n'],[0xE1C0,'Y'],
-  [0xE1C1,'1'],[0xE1D8,'K'],[0xE1E4,'M'],[0xE1EA,'Y'],[0xE215,'C'],[0xE218,'A'],
-  [0xE22B,'h'],[0xE240,'x'],[0xE248,'v'],[0xE257,'G'],[0xE27E,'b'],[0xE2A9,'B'],
-  [0xE2C5,'s'],[0xE2C7,'t'],[0xE2CA,'G'],[0xE2E3,'k'],[0xE2F8,'q'],[0xE30F,'F'],
-  [0xE311,'u'],[0xE32F,'E'],[0xE334,'2'],[0xE34A,'I'],[0xE37C,'R'],[0xE38F,'v'],
-  [0xE39B,'X'],[0xE3B0,'l'],[0xE3B7,'7'],[0xE3F1,'l'],[0xE41B,'o'],[0xE41C,'H'],
-  [0xE426,'S'],[0xE427,'J'],[0xE43E,'6'],[0xE44E,'X'],[0xE46A,'b'],[0xE477,'y'],
-  [0xE49A,'c'],[0xE4A3,'8'],[0xE4AE,'2'],[0xE4CC,'s'],[0xE4D3,'5'],[0xE4DB,'L'],
-  [0xE4DF,'N'],[0xE4EC,'5'],[0xE4F3,'r'],[0xE519,'0'],[0xE51F,'g'],[0xE550,'E'],
-  [0xE557,'h'],[0xE566,'N'],[0xE571,'F'],[0xE57B,'O'],[0xE5BD,'C'],[0xE5C1,'d'],
-  [0xE5C9,'8'],[0xE5D1,'x'],[0xE5DC,'m'],[0xE5E1,'9'],[0xE5F0,'u'],[0xE5FA,'m'],
-  [0xE5FF,'a'],[0xE603,'U'],[0xE62A,'w'],[0xE636,'P'],[0xE63E,'D'],[0xE648,'6'],
-  [0xE65B,'H'],[0xE65D,'z'],[0xE660,'9'],[0xE68D,'1'],[0xE691,'M'],[0xE6A4,'q'],
-  [0xE6A5,'c'],[0xE6D7,'W'],[0xE6E0,'R'],[0xE6F1,'T'],[0xE6F3,'a'],[0xE6F5,'g'],
-  [0xE705,'w'],[0xE71A,'3'],[0xE735,'Z'],[0xE74F,'Q'],[0xE762,'r'],[0xE765,'n'],
-  [0xE775,'V'],[0xE77A,'d'],[0xE77D,'L'],[0xE77E,'4'],[0xE7C7,'U'],[0xE7E5,'0'],
-  [0xE7F6,'7'],[0xE902,'A'],[0xE915,'O'],[0xE91F,'e'],[0xE946,'a'],[0xE95D,'2'],
-  [0xE97B,'f'],[0xE9A8,'y'],[0xE9CC,'P'],[0xE9D5,'o'],[0xE9D7,'r'],[0xE9F8,'O'],
-  [0xE9F9,'K'],[0xEA15,'e'],[0xEA20,'Y'],[0xEA24,'N'],[0xEA2D,'v'],[0xEA2E,'R'],
-  [0xEA2F,'C'],[0xEA43,'4'],[0xEA47,'l'],[0xEA65,'S'],[0xEA75,'M'],[0xEA76,'H'],
-  [0xEA77,'u'],[0xEA82,'o'],[0xEAA1,'k'],[0xEAA4,'a'],[0xEAA5,'x'],[0xEAA6,'z'],
-  [0xEAB2,'6'],[0xEAB4,'t'],[0xEABB,'y'],[0xEAC5,'w'],[0xEACF,'b'],[0xEAD5,'L'],
-  [0xEAE3,'A'],[0xEAED,'F'],[0xEB02,'s'],[0xEB06,'s'],[0xEB0E,'C'],[0xEB0F,'R'],
-  [0xEB18,'w'],[0xEB27,'D'],[0xEB62,'l'],[0xEB63,'9'],[0xEB75,'h'],[0xEB85,'X'],
-  [0xEBEC,'k'],[0xEBF6,'N'],[0xEC0F,'q'],[0xEC19,'J'],[0xEC50,'7'],[0xEC6D,'g'],
-  [0xEC75,'d'],[0xEC85,'n'],[0xECAD,'V'],[0xECB4,'S'],[0xECD4,'L'],[0xECDB,'Z'],
-  [0xECE6,'E'],[0xECF8,'U'],[0xED07,'V'],[0xED2C,'Q'],[0xED35,'l'],[0xED37,'J'],
-  [0xED48,'W'],[0xED64,'5'],[0xED71,'2'],[0xED72,'v'],[0xED8C,'E'],[0xEDEB,'Y'],
-  [0xEDEC,'5'],[0xEDED,'m'],[0xEE01,'c'],[0xEE09,'Q'],[0xEE0C,'n'],[0xEE0F,'u'],
-  [0xEE47,'W'],[0xEE5C,'P'],[0xEE69,'b'],[0xEE8D,'0'],[0xEEA1,'X'],[0xEEBB,'F'],
-  [0xEEC1,'I'],[0xEECC,'B'],[0xEECF,'c'],[0xEEDA,'1'],[0xEEDB,'D'],[0xEEE3,'G'],
-  [0xEF1F,'8'],[0xEF26,'K'],[0xEF35,'x'],[0xEF37,'6'],[0xEF3A,'d'],[0xEF57,'H'],
-  [0xEF5A,'U'],[0xEF61,'G'],[0xEF91,'8'],[0xEF94,'T'],[0xEFC8,'m'],[0xEFD4,'1'],
-  [0xEFD7,'Z'],[0xEFDA,'h'],[0xEFEE,'3'],[0xEFEF,'4'],[0xEFF6,'3'],[0xF00A,'q'],
-  [0xF019,'T'],[0xF050,'B'],[0xF065,'0'],[0xF073,'7'],[0xF096,'z'],[0xF0A6,'t'],
-  [0xF0BA,'r'],[0xF0BD,'M'],[0xF0C0,'g'],[0xF7A0,'0'],[0xF7A1,'1'],[0xF7A2,'2'],
-  [0xF7A3,'3'],[0xF7A4,'4'],[0xF7A5,'5'],[0xF7A6,'6'],[0xF7A7,'7'],[0xF7A8,'8'],
-  [0xF7A9,'9'],[0xF8FF,'*'],
+  [0xE01B, 'A'], [0xE01E, 'y'], [0xE05F, '3'], [0xE063, 'z'], [0xE06B, 'K'], [0xE06C, 't'],
+  [0xE089, 'l'], [0xE0D5, 'S'], [0xE0D6, 'T'], [0xE100, 'o'], [0xE101, 'P'], [0xE116, '4'],
+  [0xE122, 'W'], [0xE124, 'Z'], [0xE14B, 'J'], [0xE160, 'e'], [0xE184, 'O'], [0xE186, 'D'],
+  [0xE1A4, 'f'], [0xE1AD, 'e'], [0xE1B4, 'k'], [0xE1B8, 'f'], [0xE1BF, 'n'], [0xE1C0, 'Y'],
+  [0xE1C1, '1'], [0xE1D8, 'K'], [0xE1E4, 'M'], [0xE1EA, 'Y'], [0xE215, 'C'], [0xE218, 'A'],
+  [0xE22B, 'h'], [0xE240, 'x'], [0xE248, 'v'], [0xE257, 'G'], [0xE27E, 'b'], [0xE2A9, 'B'],
+  [0xE2C5, 's'], [0xE2C7, 't'], [0xE2CA, 'G'], [0xE2E3, 'k'], [0xE2F8, 'q'], [0xE30F, 'F'],
+  [0xE311, 'u'], [0xE32F, 'E'], [0xE334, '2'], [0xE34A, 'I'], [0xE37C, 'R'], [0xE38F, 'v'],
+  [0xE39B, 'X'], [0xE3B0, 'l'], [0xE3B7, '7'], [0xE3F1, 'l'], [0xE41B, 'o'], [0xE41C, 'H'],
+  [0xE426, 'S'], [0xE427, 'J'], [0xE43E, '6'], [0xE44E, 'X'], [0xE46A, 'b'], [0xE477, 'y'],
+  [0xE49A, 'c'], [0xE4A3, '8'], [0xE4AE, '2'], [0xE4CC, 's'], [0xE4D3, '5'], [0xE4DB, 'L'],
+  [0xE4DF, 'N'], [0xE4EC, '5'], [0xE4F3, 'r'], [0xE519, '0'], [0xE51F, 'g'], [0xE550, 'E'],
+  [0xE557, 'h'], [0xE566, 'N'], [0xE571, 'F'], [0xE57B, 'O'], [0xE5BD, 'C'], [0xE5C1, 'd'],
+  [0xE5C9, '8'], [0xE5D1, 'x'], [0xE5DC, 'm'], [0xE5E1, '9'], [0xE5F0, 'u'], [0xE5FA, 'm'],
+  [0xE5FF, 'a'], [0xE603, 'U'], [0xE62A, 'w'], [0xE636, 'P'], [0xE63E, 'D'], [0xE648, '6'],
+  [0xE65B, 'H'], [0xE65D, 'z'], [0xE660, '9'], [0xE68D, '1'], [0xE691, 'M'], [0xE6A4, 'q'],
+  [0xE6A5, 'c'], [0xE6D7, 'W'], [0xE6E0, 'R'], [0xE6F1, 'T'], [0xE6F3, 'a'], [0xE6F5, 'g'],
+  [0xE705, 'w'], [0xE71A, '3'], [0xE735, 'Z'], [0xE74F, 'Q'], [0xE762, 'r'], [0xE765, 'n'],
+  [0xE775, 'V'], [0xE77A, 'd'], [0xE77D, 'L'], [0xE77E, '4'], [0xE7C7, 'U'], [0xE7E5, '0'],
+  [0xE7F6, '7'], [0xE902, 'A'], [0xE915, 'O'], [0xE91F, 'e'], [0xE946, 'a'], [0xE95D, '2'],
+  [0xE97B, 'f'], [0xE9A8, 'y'], [0xE9CC, 'P'], [0xE9D5, 'o'], [0xE9D7, 'r'], [0xE9F8, 'O'],
+  [0xE9F9, 'K'], [0xEA15, 'e'], [0xEA20, 'Y'], [0xEA24, 'N'], [0xEA2D, 'v'], [0xEA2E, 'R'],
+  [0xEA2F, 'C'], [0xEA43, '4'], [0xEA47, 'l'], [0xEA65, 'S'], [0xEA75, 'M'], [0xEA76, 'H'],
+  [0xEA77, 'u'], [0xEA82, 'o'], [0xEAA1, 'k'], [0xEAA4, 'a'], [0xEAA5, 'x'], [0xEAA6, 'z'],
+  [0xEAB2, '6'], [0xEAB4, 't'], [0xEABB, 'y'], [0xEAC5, 'w'], [0xEACF, 'b'], [0xEAD5, 'L'],
+  [0xEAE3, 'A'], [0xEAED, 'F'], [0xEB02, 's'], [0xEB06, 's'], [0xEB0E, 'C'], [0xEB0F, 'R'],
+  [0xEB18, 'w'], [0xEB27, 'D'], [0xEB62, 'l'], [0xEB63, '9'], [0xEB75, 'h'], [0xEB85, 'X'],
+  [0xEBEC, 'k'], [0xEBF6, 'N'], [0xEC0F, 'q'], [0xEC19, 'J'], [0xEC50, '7'], [0xEC6D, 'g'],
+  [0xEC75, 'd'], [0xEC85, 'n'], [0xECAD, 'V'], [0xECB4, 'S'], [0xECD4, 'L'], [0xECDB, 'Z'],
+  [0xECE6, 'E'], [0xECF8, 'U'], [0xED07, 'V'], [0xED2C, 'Q'], [0xED35, 'l'], [0xED37, 'J'],
+  [0xED48, 'W'], [0xED64, '5'], [0xED71, '2'], [0xED72, 'v'], [0xED8C, 'E'], [0xEDEB, 'Y'],
+  [0xEDEC, '5'], [0xEDED, 'm'], [0xEE01, 'c'], [0xEE09, 'Q'], [0xEE0C, 'n'], [0xEE0F, 'u'],
+  [0xEE47, 'W'], [0xEE5C, 'P'], [0xEE69, 'b'], [0xEE8D, '0'], [0xEEA1, 'X'], [0xEEBB, 'F'],
+  [0xEEC1, 'I'], [0xEECC, 'B'], [0xEECF, 'c'], [0xEEDA, '1'], [0xEEDB, 'D'], [0xEEE3, 'G'],
+  [0xEF1F, '8'], [0xEF26, 'K'], [0xEF35, 'x'], [0xEF37, '6'], [0xEF3A, 'd'], [0xEF57, 'H'],
+  [0xEF5A, 'U'], [0xEF61, 'G'], [0xEF91, '8'], [0xEF94, 'T'], [0xEFC8, 'm'], [0xEFD4, '1'],
+  [0xEFD7, 'Z'], [0xEFDA, 'h'], [0xEFEE, '3'], [0xEFEF, '4'], [0xEFF6, '3'], [0xF00A, 'q'],
+  [0xF019, 'T'], [0xF050, 'B'], [0xF065, '0'], [0xF073, '7'], [0xF096, 'z'], [0xF0A6, 't'],
+  [0xF0BA, 'r'], [0xF0BD, 'M'], [0xF0C0, 'g'], [0xF7A0, '0'], [0xF7A1, '1'], [0xF7A2, '2'],
+  [0xF7A3, '3'], [0xF7A4, '4'], [0xF7A5, '5'], [0xF7A6, '6'], [0xF7A7, '7'], [0xF7A8, '8'],
+  [0xF7A9, '9'], [0xF8FF, '*'],
 ] as [number, string][]).forEach(([code, ch]) => {
   GLYPH_MAP[String.fromCharCode(code)] = ch;
 });
@@ -344,8 +344,11 @@ function wrapWithParagraphs(rawText: string): string {
 class STVChapterError extends Error {
   public errorCode: number;
   public raw: any;
-  constructor(message: string, code: number, detail: any) {
-    super(`${message} (code ${code})`);
+  constructor(code: number, detail: any) {
+    super(
+      `${STVChapterError.getMessage(code)} (code ${code})\n` +
+        STVChapterError.stringifyJson(detail),
+    );
     this.name = 'STVChapterError';
     this.errorCode = code;
     this.raw = detail;
@@ -355,9 +358,50 @@ class STVChapterError extends Error {
     switch (this.errorCode) {
       case 0: // OK
       case 1: // empty
+      case 12:
+      case 13:
+      case 15:
+      case 18:
+      case 19:
+      case 21:
+      case 101:
         return true;
       default:
         return false;
+    }
+  }
+  static stringifyJson(data: any) {
+    try {
+      return JSON.stringify(data);
+    } catch {
+      return `${data}`;
+    }
+  }
+  static getMessage(code: number | string) {
+    code = code.toString();
+    switch (code) {
+      case '1':
+        return 'Chương không có nội dung.';
+      case '5':
+        return 'Lỗi không xác định.';
+      case '7':
+        return 'Bạn đang tải chương quá nhanh. Hãy thử lại sau vài giây.';
+      case '12':
+        return 'Bạn chưa mua chương ở sfacg. Đăng nhập để tiếp tục.';
+      case '13':
+        return 'Bạn chưa đăng nhập.';
+      case '15':
+        return 'Đang đặt location chuyển hướng.';
+      case '18':
+        return 'Yêu cầu chuyển hướng.';
+      case '19':
+        return 'Có lỗi không xác định. Yêu cầu chuyển hướng';
+      case '21':
+        return 'Bạn cần xác nhận captcha. Hãy thử lại sau vài giây.';
+      case '101':
+        return 'Truyện này không phải novel (type=manga)';
+      default:
+        return 'Unexpected response.';
     }
   }
 }
@@ -371,7 +415,7 @@ class SangTacVietPlugin implements Plugin.PluginBase {
   get site() {
     return DOMAINS[this.selectedDomain] || SITE;
   }
-  version = '1.0.17';
+  version = '1.0.18';
   webStorageUtilized = true;
 
   pluginSettings: Plugin.PluginSettings = {
@@ -839,34 +883,11 @@ class SangTacVietPlugin implements Plugin.PluginBase {
     } else {
       console.warn('Unexpected chapter API response', data);
       switch (String(data.code)) {
-        case '7': {
-          throw new STVChapterError(
-            'Bạn đang tải chương quá nhanh. Hãy thử lại sau vài giây.',
-            7,
-            data,
-          );
-        }
         case '21': {
-          /*
-          throw new STVChapterError(
-            'Bạn cần xác nhận captcha. Hãy thử lại sau vài giây.',
-            21,
-            data,
-          );
-          */
           return "<div id='captcha-placeholder'></div><meta id='no-cache-marker'/><meta id='no-prefetch-marker'/>";
         }
-        case '1': {
-          throw new STVChapterError(
-            typeof data.err === 'string'
-              ? data.err
-              : 'Chương không có nội dung.',
-            1,
-            data,
-          );
-        }
         default: {
-          throw new Error(`Unexpected response: ${JSON.stringify(data)}`);
+          throw new STVChapterError(Number(data.code), data);
         }
       }
     }
