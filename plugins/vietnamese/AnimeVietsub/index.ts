a776debee35a662e8e2e9f644b3b584d5eb824da
Elysia <71698422+aiko-chan-ai@users.noreply.github.com>
2026-05-29 22:06:09 +0700

    bump

--- plugins/vietnamese/AnimeVietsub/index.ts
+++ plugins/vietnamese/AnimeVietsub/index.ts
@@ -13,7 +13,7 @@ class AnimeVietsubPlugin implements Plugin.PluginBase {
   name = 'AnimeVietsub';
   icon = 'src/vi/animevietsub/icon.png';
   site = 'https://animevietsub.site';
-  version = '1.0.26';
+  version = '1.0.27';
   filters = filters;
   customJS = 'src/vi/animevietsub/player.js';
   customCSS = 'src/vi/animevietsub/custom.css';
