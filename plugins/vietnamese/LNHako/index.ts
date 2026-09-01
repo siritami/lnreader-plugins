1617243b048441cbe2d8552b1503578bbea6de91
siritami <103488094+siritami@users.noreply.github.com>
2026-09-02 00:59:12 +0700

    1

--- plugins/vietnamese/LNHako/index.ts
+++ plugins/vietnamese/LNHako/index.ts
@@ -16,7 +16,7 @@ class HakoPlugin implements Plugin.PluginBase {
   id = 'ln.hako.vn';
   name = 'Hako Novel';
   icon = 'src/vi/hakolightnovel/icon.png';
-  version = '1.2.19';
+  version = '1.2.20';
   filters = filters;
 
   customCSS = 'src/vi/hakolightnovel/custom.css';
